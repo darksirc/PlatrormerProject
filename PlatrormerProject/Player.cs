86b7ba1b09eebf012362ca63b81f03a4f5863a85
darksirc <36098449+darksirc@users.noreply.github.com>
2018-04-09 18:24:08 +1000

    ?
    ?

---
+++ PlatrormerProject/Player.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using PlatformerProject;
+
+namespace PlatrormerProject
+{
+    class Player
+    {
+
+        Sprite playerSprite = new Sprite();
+
+        // functions
+
+        public Player()
+        {
+        }
+        public void Load(ContentManager content)
+        {
+            playerSprite.Load(content, "hero");
+        }
+        public void Update(float deltaTime)
+        {
+            playerSprite.Update(deltaTime);
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            playerSprite.Draw(spriteBatch);
+        }
+    }
+}
